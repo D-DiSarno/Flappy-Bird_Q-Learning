608abbbf17727abfb7299e572670a37511d39274
drybonez01 <83592470+drybonez01@users.noreply.github.com>
2023-11-25 14:11:40 +0100

    updated train.py

--- main1.py
+++ main1.py
@@ -2,10 +2,9 @@ from ple.games.flappybird import FlappyBird
 from ple import PLE
 from tqdm import tqdm
 import torch
-#from models.q_learning import QLearning
+# from models.q_learning import QLearning
 from models.deep_q_learning import DeepQLearning
 from models.q_learning_optimized import QLearning
-
 from flappy_agent import FlappyAgent
 from train import train
 
@@ -15,26 +14,16 @@ def run_game(nb_episodes, agent):
     An episode of FlappyBird ends with the bird crashing into a pipe or going off screen.
     """
 
-    reward_values = {
-        "positive": 1.0,
-        "negative": 0.0,
-        "tick": 0.0,
-        "loss": 0.0,
-        "win": 0.0,
-    }
-    # TODO: when training use the following instead:
-    #reward_values = agent.reward_values
+    reward_values = agent.reward_values()
 
     env = PLE(
         FlappyBird(),
         fps=30,
-        display_screen=False,
-        force_fps=True,
-        rng= None ,
+        display_screen=True,
+        force_fps=False,
+        rng=None,
         reward_values=reward_values,
     )
-    # TODO: to speed up training change parameters of PLE as follows:
-    # display_screen=False, force_fps=True
     env.init()
     scores = []
     total = nb_episodes
@@ -46,10 +35,9 @@ def run_game(nb_episodes, agent):
             # pick an action
             # TODO: for training using agent.training_policy instead
             action = agent.policy(env.game.getGameState())
-            #action = agent.training_policy(env.game.getGameState())
+            # action = agent.training_policy(env.game.getGameState())
             # step the environment
             reward = env.act(env.getActionSet()[action])
-            print("reward=%d" % reward)
 
             # TODO: for training let the agent observe the current state transition
 
@@ -58,15 +46,12 @@ def run_game(nb_episodes, agent):
             # reset the environment if the game is over
             if env.game_over():
                 if score > 1000 and score > max_score:
-                    agent.save_model()
+                    # agent.save_model()
                     max_score = score
 
                 if nb_episodes % 20 == 0 and scores != []:
                     print(
-                        f"Max score for {total - nb_episodes} episodes: {max(scores)}"
-                    )
-                    print(
-                        f"Avg score for {total - nb_episodes} episodes: {sum(scores) / len(scores)}"
+                        f" [MAX: {max(scores)}] [AVG: {sum(scores) / len(scores)}]"
                     )
 
                 pbar.update(1)
@@ -74,8 +59,9 @@ def run_game(nb_episodes, agent):
                 env.reset_game()
                 nb_episodes -= 1
                 score = 0
-    print(f"90% over 50: {check_ninety_percent_over_fifty(scores)}")
+    print(f"\n90% over 50: {check_ninety_percent_over_fifty(scores)}")
     print(f"Max Test score: {max(scores)}")
+
 
 def check_ninety_percent_over_fifty(array):
     total_elements = len(array)
@@ -84,12 +70,12 @@ def check_ninety_percent_over_fifty(array):
     return percentage_over_fifty >= 90
 
 
-
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    agent = FlappyAgent(learner=DeepQLearning(8,2))
+    # agent = FlappyAgent(learner=DeepQLearning(8,2))
+    agent = FlappyAgent(learner=QLearning())
 
-    train(25000, agent)
-    # agent.load_model("./models/q_table18000.json")
-    #run_game(100, agent)
+    # train(30000, agent)
+    agent.load_model("q_table30000.json")
+    run_game(100, agent)
--- train.py
+++ train.py
@@ -5,12 +5,13 @@ from ple.games.flappybird import FlappyBird
 from ple import PLE
 from tqdm import tqdm
 
+
 def train(nb_episodes, agent):
     reward_values = agent.reward_values()
 
     env = PLE(
         FlappyBird(),
-        fps=30,#di base 30
+        fps=30,  # di base 30
         display_screen=False,
         force_fps=True,
         rng=None,
@@ -35,8 +36,8 @@ def train(nb_episodes, agent):
 
             # step the environment
             reward = env.act(env.getActionSet()[action])
-            if reward>10:
-              print("reward=%d" % reward)
+            if reward > 10:
+                print("reward=%d" % reward)
 
             # let the agent observe the current state transition
             newState = env.game.getGameState()
@@ -49,11 +50,7 @@ def train(nb_episodes, agent):
             if env.game_over():
                 if nb_episodes % 1000 == 0 and scores != []:
                     print(
-                        f"Max score for {total - nb_episodes} episodes: {max(scores)}"
-                    )
-
-                    print(
-                        f"Avg score for {total - nb_episodes} episodes: {sum(scores) / len(scores)}"
+                        f" [MAX: {max(scores)}] [AVG: {sum(scores) / len(scores)}]"
                     )
                     plot_scores.append(score)
                     mean_score = sum(scores) / len(scores)
@@ -69,15 +66,11 @@ def train(nb_episodes, agent):
                 env.reset_game()
                 pbar.update(1)
                 nb_episodes -= 1
-
-                #########
-
-
-
                 score = 0
 
-        print(f"Max Train score: {max(scores)}")
+        print(f"\nMax Train score: {max(scores)}")
         print(f"Avg score for {total} episodes: {sum(scores) / len(scores)}")
+        agent.save_model(filename="q_table" + str(total) + ".json")
 
     plt.plot(total_rewards, label="Total Reward per Episode")
     plt.xlabel("Episode")
@@ -87,4 +80,3 @@ def train(nb_episodes, agent):
     plt.show()
 
     print(f"Number of Frames: {frames}")
-
